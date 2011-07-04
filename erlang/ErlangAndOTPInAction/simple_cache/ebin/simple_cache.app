d100a496f7d92d012a110940a27a0a77084d0a1b
KK <kk.questworld@gmail.com>
2011-07-04 08:28:48 +0530

    Switched to Mnesia, integrated simple_cache with resource_discovery

--- erlang/ErlangAndOTPInAction/simple_cache/ebin/simple_cache.app
+++ erlang/ErlangAndOTPInAction/simple_cache/ebin/simple_cache.app
@@ -1,12 +1,18 @@
 {application, simple_cache,
  [{description, "A simple cache, written by KK"},
-  {vsn, "0.1.0"},
+  {vsn, "0.2.0"},
   {modules, [
              sc_app,
-             sc_sup
+             sc_sup,
+             sc_element_sup,
+             sc_store,
+             sc_element,
+             sc_event,
+             mod_stats,
+             mod_stats_event_hooks
             ]},
   {registered, [sc_sup]},
-  {applications, [kernel, stdlib]},
+  {applications, [kernel, stdlib, sasl, mnesia, resource_discovery]},
   {mod, {sc_app, []}}
  ]
 }.
--- erlang/ErlangAndOTPInAction/simple_cache/src/sc_store.erl
+++ erlang/ErlangAndOTPInAction/simple_cache/src/sc_store.erl
@@ -8,29 +8,61 @@
 
 
 -define(TABLE_ID, simple_cache).
-
+-define(WAIT_FOR_TABLES, 5000).
+-record(key_to_pid, {key, pid}).
 
 init() ->
-    ets:new(?TABLE_ID, [public, named_table, set]),
-    ok.
+    mnesia:stop(),
+    mnesia:delete_schema([node()]),
+    mnesia:start(),
+    {ok, CacheNodes} = rd_server:fetch_resources(simple_cache),
+    dynamic_db_init(lists:delete(node(), CacheNodes)).
+
+dynamic_db_init([]) ->
+    mnesia:create_table(key_to_pid,
+                       [{index, [pid]},
+                        {attributes, record_info(fields, key_to_pid)}
+                       ]);
+dynamic_db_init(CacheNodes) ->
+    add_extra_nodes(CacheNodes).
+
+add_extra_nodes([Node | T]) ->
+    case mnesia:change_config(extra_db_nodes, [Node]) of
+        {ok, [Node]} ->
+            mnesia:add_table_copy(schema, node(), ram_copies),
+            mnesia:add_table_copy(key_to_pid, node(), ram_copies),
+            Tables = mnesia:system_info(tables),
+            mnesia:wait_for_tables(Tables, ?WAIT_FOR_TABLES);
+        _ ->
+            add_extra_nodes(T)
+    end.
 
 insert(Key, Pid) ->
-    ets:insert(?TABLE_ID, {Key, Pid}).
+    mnesia:dirty_write(#key_to_pid{key=Key, pid=Pid}).
+
+is_pid_alive(Pid) when node(Pid) =:= node() ->
+    is_process_alive(Pid);
+is_pid_alive(Pid) ->
+    lists:member(node(Pid), nodes()) andalso
+        (rpc:call(node(Pid), erlang, is_process_alive, [Pid]) =:= true).
 
 lookup(Key) ->
-    case ets:lookup(?TABLE_ID, Key) of
-        [{Key, Pid}] ->
-            {ok, Pid};
+    case mnesia:dirty_read(key_to_pid, Key) of
+        [{key_to_pid, Key, Pid}] ->
+            case is_pid_alive(Pid) of
+                true -> {ok, Pid};
+                false -> {error, not_found}
+            end;
         [] ->
             {error, not_found}
     end.
 
 delete(Pid) ->
-    case ets:match(?TABLE_ID, {'$1', Pid}) of
-        [[Key]] ->
+    case mnesia:dirty_index_read(key_to_pid, Pid, #key_to_pid.pid) of
+        [#key_to_pid{key=Key} = Record] ->
             sc_event:delete(Key),
-            ets:delete(?TABLE_ID, Key);
-        [] ->
-            true
+            mnesia:dirty_delete_object(Record);
+        _ ->
+            ok
     end.
             
